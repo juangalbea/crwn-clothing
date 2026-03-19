8ac042cc2dc0aa14705cd741f191a71b23b71e29
Juan Garcia <juangarcia@Juans-MacBook-Air.local>
2026-03-19 11:29:03 +0000

    feat: added links to the sections in shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
@@ -7,8 +8,15 @@ import './collection-preview.styles.scss';
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <div className='title-row'>
-      <h1 className='title'>{title}</h1>
-      <span className='item-count'>{items.length} pieces</span>
+      <div className='title-group'>
+        <Link className='title' to={`/shop/${title.toLowerCase()}`}>
+          {title}
+        </Link>
+        <span className='item-count'>{items.length} pieces</span>
+      </div>
+      <Link className='view-all-link' to={`/shop/${title.toLowerCase()}`}>
+        View Collection
+      </Link>
     </div>
     <div className='preview'>
       {items
